63c9c752cf6e1ca2200cb66c859b0eff18a30d5a
prabhata kumar maharana <maharanakumar2@outlook.com>
2016-12-24 17:11:17 +0530

    Create My Blog

---
+++ DotNetBlog/Controllers/MyBlogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetBlog.Controllers
+{
+    public class MyBlogController : Controller
+    {
+        // GET: MyBlog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
