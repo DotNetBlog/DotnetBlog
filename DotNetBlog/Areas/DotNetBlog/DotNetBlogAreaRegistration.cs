0299c7e074f7517641ef8b6d05d31374d35ee351
prabhata kumar maharana <maharanakumar2@outlook.com>
2017-01-01 19:32:06 +0530

    I'm Added new Area,Shared,Css,Script floder in my project..also included Bootstrap file in my project.

---
+++ DotNetBlog/Areas/DotNetBlog/DotNetBlogAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DotNetBlog.Areas.DotNetBlog
+{
+    public class DotNetBlogAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DotNetBlog";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DotNetBlog_default",
+                "DotNetBlog/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
